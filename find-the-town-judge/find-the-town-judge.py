6080af89d2a5f6a71a6c5617c8931b22c87e04d1
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2022-01-03 12:28:17 +0530

    Time: 949 ms (14.13%), Space: 18.9 MB (65.18%) - LeetHub

---
+++ find-the-town-judge/find-the-town-judge.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        degree = [0]*(n+1)
+        for x, y in trust:
+            degree[x] -= 1
+            degree[y] += 1
+        print(degree)
+        for i in range(1, n+1):
+            if degree[i] == n-1:
+                return i
+        return -1
+            
