639560b9326cad336c235635c1f01cf7d563e632
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2022-01-03 15:24:02 +0530

    Time: 114 ms (63.92%), Space: 14.1 MB (96.14%) - LeetHub

---
+++ best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        def maxProfitAfterKTrans(prices, K):
+            buy = [float(inf)]*(K+1)
+            sell = [0]*(K+1)
+            for p in prices:
+                for k in range(1, K+1):
+                    buy[k] = min(buy[k], p-sell[k-1])
+                    sell[k] = max(sell[k], p-buy[k])
+            return sell[-1]
+        
+        # O(k*n), O(k)
+        return maxProfitAfterKTrans(prices, k)
