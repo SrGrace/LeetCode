bd605010d32af11632c98856d61af100a47f5c38
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2022-01-04 14:14:19 +0530

    Time: 36 ms (12.93%), Space: 14.1 MB (89.54%) - LeetHub

--- complement-of-base-10-integer/complement-of-base-10-integer.py
+++ complement-of-base-10-integer/complement-of-base-10-integer.py
@@ -1,11 +1,21 @@
 class Solution:
     def bitwiseComplement(self, n: int) -> int:
-        def bin2dec(binary):
-            decimal = 0
-            for digit in binary:
-                decimal = decimal*2 + int(digit)
-            return decimal
+        # def bin2dec(binary): 
+        #     decimal = 0
+        #     for digit in binary:
+        #         decimal = decimal*2 + int(digit)
+        #     return decimal
+        #
+        # bin_len = len(bin(n)[2:]) # O(logn)
+        # # print(bin_len, '1'*bin_len, bin2dec('1'*bin_len))
+        # # O(logn*len(bin_len))
+        # comp = bin2dec('1'*bin_len) - n  # comp = dec(full_bin - bin)
+        # return comp
         
-        bin_len = len(bin(n)[2:])
-        # print(bin_len, '1'*bin_len, bin2dec('1'*bin_len))
-        return bin2dec('1'*bin_len) - n
+        # O(logn)
+        p = 2  # smallest power of 2 larger than n
+        while p <= n:
+            p *= 2
+        return p - 1 - n  # comp = p-1-n
+    
+        
