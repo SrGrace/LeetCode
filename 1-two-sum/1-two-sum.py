d8845ef63376582d0e587e531bd912835bf7ea52
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2022-01-06 10:40:10 +0530

    Time: 96 ms (45.52%), Space: 15.3 MB (54.14%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hsh = dict()
+        for i in range(len(nums)):
+            if target - nums[i] in hsh:
+                return [i, hsh[target - nums[i]]]
+            else:
+                hsh[nums[i]] = i
+        return -1
