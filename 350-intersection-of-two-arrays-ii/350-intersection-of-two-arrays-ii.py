689106182851975be7826519089dd59be1d67af6
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2022-01-07 11:25:46 +0530

    Time: 75 ms (16.81%), Space: 14.4 MB (43.86%) - LeetHub

---
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        hsh = dict()
+        for n in nums1:
+            if n in hsh:
+                hsh[n] += 1
+            else:
+                hsh[n] = 1
+        res = list()
+        for n in nums2:
+            if n in hsh and hsh[n] > 0:
+                res.append(n)
+                hsh[n] -= 1
+        return res
+    
