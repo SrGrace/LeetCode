ec5685bd360ca7448324775d4c9639f7d63bd372
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2022-01-07 12:35:47 +0530

    Time: 149 ms (28.87%), Space: 17.4 MB (56.84%) - LeetHub

--- 382-linked-list-random-node/382-linked-list-random-node.py
+++ 382-linked-list-random-node/382-linked-list-random-node.py
@@ -6,14 +6,25 @@
 class Solution:
 
     def __init__(self, head: Optional[ListNode]):
-        self.range = list()
-        while(head):
-            self.range.append(head.val)
-            head = head.next
+        # self.range = list()
+        # while(head):
+        #     self.range.append(head.val)
+        #     head = head.next
+        
+        self.head = head
 
     def getRandom(self) -> int:
-        idx = int(random.random()*len(self.range))
-        return self.range[idx]
+        # idx = int(random.random()*len(self.range))
+        # return self.range[idx]
+        
+        cur = self.head
+        res, k = 0, 1  # rand element (choosen val), k element (1 in this case, the scope)
+        while(cur):
+            if random.random() < 1/k:
+                res = cur.val
+            k += 1
+            cur = cur.next
+        return res
 
 
 # Your Solution object will be instantiated and called as such:
