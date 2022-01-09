8cbed480ec4081906e3e3a6a508d962b5d258b37
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2022-01-09 09:49:40 +0530

    Time: 96 ms (76.01%), Space: 14.2 MB (70.99%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        seen = []
+        for i, row in enumerate(board):
+            for j, c in enumerate(row):
+                if c != '.':
+                    seen += [(c,j),(i,c),(i//3,j//3,c)]
+        return len(seen) == len(set(seen))
+    
