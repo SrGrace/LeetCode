b76928664c9c374034abff4d96b30cf5a8cedfaf
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2022-01-10 13:52:42 +0530

    Time: 60 ms (36.31%), Space: 14.3 MB (95.49%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return 0
+        cnt = collections.Counter(s)
+        for idx, ch in enumerate(t):
+            cnt[ch] -= 1
+            if cnt[ch] < 0:
+                return 0
+        return 1
