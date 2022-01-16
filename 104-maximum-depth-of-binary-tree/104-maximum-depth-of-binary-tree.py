387160c04ecd05665cefbbb41c1961022365dda6
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2022-01-16 10:32:29 +0530

    Time: 32 ms (98.51%), Space: 16.1 MB (41.55%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
@@ -6,21 +6,31 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
+        # if not root:
+        #     return 0
         
-        max_cnt = []
-        self._path(root, 0, max_cnt)
-        return max(max_cnt)
+        # max_cnt = []
+        # self._path(root, 0, max_cnt)
+        # return max(max_cnt)
+        
+        return self.dfs(root, 0)
     
-    def _path(self, root, cnt, max_cnt):
-        cnt += 1
-        if root.left:
-            self._path(root.left, cnt, max_cnt)
-        if root.right:
-            self._path(root.right, cnt, max_cnt)
+    # def _path(self, root, cnt, max_cnt):
+        # cnt += 1
+        # if root.left:
+        #     self._path(root.left, cnt, max_cnt)
+        # if root.right:
+        #     self._path(root.right, cnt, max_cnt)
         
-        if not root.left and not root.right:
-            max_cnt.append(cnt)
+        # if not root.left and not root.right:
+        #     max_cnt.append(cnt)
+        
+    def dfs(self, root, depth):
+        if not root:
+            return depth
+        left_depth = self.dfs(root.left, depth+1)
+        right_depth = self.dfs(root.right, depth+1)
+        
+        return left_depth if left_depth > right_depth else right_depth
         
         
