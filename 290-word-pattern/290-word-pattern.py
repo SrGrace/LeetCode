e386a9748ed8ae86ab9bf15a980ca31bb8e5a40a
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2022-01-17 14:28:12 +0530

    Time: 56 ms (7.34%), Space: 14.1 MB (94.65%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.py
@@ -0,0 +1,19 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        st = s.split(' ')
+        
+        if len(pattern) != len(st):
+            return 0
+        if len(set(pattern)) != len(set(st)):
+            return 0
+        
+        hsh = dict()
+        for i in range(len(pattern)):
+            if pattern[i] in hsh and hsh[pattern[i]] != st[i]:
+                return 0
+            else:
+                hsh[pattern[i]] = st[i]
+        
+        return 1
+        
+    
