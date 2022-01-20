7c9d1c45d386473ad635852a6599510afe243af8
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2022-01-20 12:05:02 +0530

    Time: 46 ms (46.99%), Space: 14.4 MB (35.43%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.py
+++ 410-split-array-largest-sum/410-split-array-largest-sum.py
@@ -3,22 +3,30 @@ class Solution:
         def min_subarrays_required(max_sum_allowed):
             cur_sum, split_req = 0, 0
             for n in nums:
-                if cur_sum + n <= max_sum_allowed:
-                    cur_sum += n
-                else:
+                # if cur_sum + n <= max_sum_allowed:
+                cur_sum += n
+                # else:
+                if cur_sum > max_sum_allowed:
                     cur_sum = n
                     split_req += 1
-            return split_req + 1
+                    if split_req + 1 > m:
+                        return 0
+                    
+            return 1  # split_req + 1
         
         l, r = max(nums), sum(nums)
-        while(l <= r):
+        while(l < r):
             max_sum_allowed = l + (r-l)//2
-            if min_subarrays_required(max_sum_allowed) <= m:
-                r = max_sum_allowed - 1
-                min_largest_split_sum = max_sum_allowed
+            # if min_subarrays_required(max_sum_allowed) <= m:
+            #     r = max_sum_allowed - 1
+            #     min_largest_split_sum = max_sum_allowed
+            if min_subarrays_required(max_sum_allowed):
+                r = max_sum_allowed
+                
             else:
                 l = max_sum_allowed + 1
+                
         # O(nlog(sum(nums))), O(1)
-        return min_largest_split_sum
+        return l  # min_largest_split_sum
     
     
