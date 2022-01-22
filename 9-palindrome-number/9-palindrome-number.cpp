b6dc1714caa9e1fea011e46326834846726b93e0
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2022-01-22 13:15:27 +0530

    Time: 0 ms (100.00%), Space: 6 MB (31.14%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string xx = to_string(x);
-        int l = 0, r = xx.length()-1;
-        while(l < r){
-            if(xx[l++] != xx[r--]){
-                return false;
-            }
-        }
-        return true;
+        // string xx = to_string(x);
+        // int l = 0, r = xx.length()-1;
+        // while(l < r){
+        //     if(xx[l++] != xx[r--]){
+        //         return false;
+        //     }
+        // }
+        // return true;
         
+        if(x < 0 or (x%10 == 0 and x != 0))
+            return false;
+        
+        int rev = 0;
+        while(x > rev){
+            rev = rev * 10 + x%10;
+            x /= 10;
+        }
+        return rev == x or x == rev/10;
     }
 };
