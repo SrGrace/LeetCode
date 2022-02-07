d7c89b145b8a548b5eeb6eb8b91441a312f1e87f
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2022-02-07 16:07:09 +0530

    Time: 46 ms (47.56%), Space: 14 MB (79.84%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        hsh = collections.Counter(s)
+        for ch in t:
+            if ch in hsh and hsh[ch] > 0:
+                hsh[ch] -= 1
+            else:
+                return ch
+        
