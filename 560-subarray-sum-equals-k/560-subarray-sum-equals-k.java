10966af979a54b4ee925b09cd780823dbf22a33f
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2022-02-10 14:40:24 +0530

    Time: 42 ms (44.57%), Space: 60.9 MB (13.70%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -1,5 +1,7 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
+        /*
+        // O(n^2) O(1)
         int cnt = 0;
         for(int st=0; st<nums.length; st++){
             int sum = 0;
@@ -8,6 +10,19 @@ class Solution {
                 if(sum == k)
                     cnt++;
             }
+        }
+        return cnt;
+        */
+        
+        //O(n), O(n)
+        int cnt = 0, sum = 0;
+        HashMap<Integer, Integer> hsh = new HashMap <> ();
+        hsh.put(0, 1);
+        for(int i=0; i<nums.length; i++){
+            sum += nums[i];
+            if(hsh.containsKey(sum-k))
+                cnt += hsh.get(sum-k);
+            hsh.put(sum, hsh.getOrDefault(sum, 0)+1);
         }
         return cnt;
     }
