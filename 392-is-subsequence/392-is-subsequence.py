df1298a43ff12726f69a157491669752ce2ac526
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2022-03-02 11:48:42 +0530

    Time: 24 ms (99.32%), Space: 14 MB (55.23%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        st = 0
+        for i in range(len(s)):
+            # print("before", st)
+            st = t.find(s[i], st)
+            # print("after", st)
+            if st == -1:
+                return False
+            st += 1
+        return True
+    
+    
