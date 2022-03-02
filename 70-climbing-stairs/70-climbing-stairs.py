95031fc8f9c4848855c91fd00d9de87f592a1e5a
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2022-03-02 16:48:36 +0530

    Time: 52 ms (24.84%), Space: 13.8 MB (80.42%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -3,10 +3,20 @@ class Solution:
         if n == 1:
             return 1
         
-        dp = [0]*n
-        dp[0], dp[1] = 1, 2
-        for i in range(2, n):
-            dp[i] = dp[i-1] + dp[i-2]
+        # O(n), O(n)
+        # dp = [0]*n
+        # dp[0], dp[1] = 1, 2
+        # for i in range(2, n):
+        #     dp[i] = dp[i-1] + dp[i-2]
+        #
+        # return dp[-1]
         
-        return dp[-1]
+        # O(n), O(1)
+        a, b = 1, 2
+        for i in range(2, n):
+            tmp = b
+            b = a+b
+            a = tmp
+        return b
+    
     
