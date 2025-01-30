67c88c83da00b013d2bbf3d72c73cb555faa897e
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2025-01-30 22:42:33 +0530

    Update 33. Search in Rotated Sorted Array.py

--- 33. Search in Rotated Sorted Array/33. Search in Rotated Sorted Array.py
+++ 33. Search in Rotated Sorted Array/33. Search in Rotated Sorted Array.py
@@ -7,12 +7,14 @@ class Solution:
             if nums[mid] == target:
                 return mid
 
+            # Check if left half is sorted
             if nums[l] <= nums[mid]:
                 if nums[l] <= target < nums[mid]:
                     r = mid - 1
                 else:
                     l = mid + 1
-            
+                    
+            # Check if right half is sorted
             else:
                 if nums[mid] < target <= nums[r]:
                     l = mid + 1
