34390d7ae967580327b86104034ea7ac598609f1
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2025-01-31 23:29:15 +0530

    Update maximum-subarray.py

--- maximum-subarray/maximum-subarray.py
+++ maximum-subarray/maximum-subarray.py
@@ -4,10 +4,10 @@ class Solution:
         # for i in range(1, len(nums)):
         #     cur_max = max(nums[i], cur_max+nums[i])
         #     max_so_far = max(max_so_far, cur_max)
-        # return max_so_far
+        # return max_so_far # O(n)
         
         for i in range(1, len(nums)):
             if nums[i-1] > 0:
                 nums[i] += nums[i-1]
-        return max(nums)
+        return max(nums) # O(n)
       
