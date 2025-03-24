842ab2b63ad0c88c159c225bf763e7382caae972
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2025-03-24 13:25:24 +0530

    Update 1094-car-pooling.py

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -3,10 +3,11 @@ class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
         mp = defaultdict(int)
         for t in trips:
-            mp[t[1]] += t[0]
-            mp[t[2]] -= t[0]
+            mp[t[1]] += t[0] # passenger get on
+            mp[t[2]] -= t[0] # passenger get off
         
         print(mp)
+        # sort by location
         mp = sorted(mp.items(), key=lambda x: x[0])
         print(mp)
             
@@ -14,5 +15,5 @@ class Solution:
             capacity -= v
             if capacity < 0:
                 return False
-        return True
+        return True  # O(nlogn), O(n)
     
