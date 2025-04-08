c9d76751dd83b1db6355c1367ca02b59e9688a3c
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2025-04-08 21:13:37 +0530

    Update 520-detect-capital.py

--- 520-detect-capital/520-detect-capital.py
+++ 520-detect-capital/520-detect-capital.py
@@ -2,6 +2,6 @@ class Solution:
     def detectCapitalUse(self, word: str) -> bool:
         if word.islower() or word.isupper() or (word[0].isupper() and word[1:].islower()):
             return True
-        return False
+        return False # O(n), O(1)
     
     
