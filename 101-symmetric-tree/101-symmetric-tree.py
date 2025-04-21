5a21232fa2e5436f62062bf544aad7d95d05a690
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2025-04-21 12:06:25 +0530

    Update 101-symmetric-tree.py

--- 101-symmetric-tree/101-symmetric-tree.py
+++ 101-symmetric-tree/101-symmetric-tree.py
@@ -6,7 +6,7 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        return self.isMirror(root, root)
+        return self.isMirror(root, root) # O(n), O(h); h (height of the tree) - logn (if balanced tree) or, n (if skewed)
     
     def isMirror(self, root1, root2):
         if not root1 and not root2:
@@ -15,4 +15,4 @@ class Solution:
             if root1.val == root2.val:
                 return (self.isMirror(root1.left, root2.right) and 
                         self.isMirror(root1.right, root2.left))
-        return False
+        return False 
