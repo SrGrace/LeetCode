42d79594b60a2eca1c95f190cb2a1ab458abb3b3
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2025-04-23 20:11:28 +0530

    Update 1845-Seat-Reservation-Manager.py

--- 1845.Seat-Reservation-Manager/1845-Seat-Reservation-Manager.py
+++ 1845.Seat-Reservation-Manager/1845-Seat-Reservation-Manager.py
@@ -2,14 +2,14 @@ class SeatManager:
 
     def __init__(self, n: int):
         self.available_seats = [i for i in range(1, n+1)]
-        # heapq.heapify(self.available_seats)
+        # heapq.heapify(self.available_seats) # O(n), O(n)
 
-    def reserve(self) -> int:
+    def reserve(self) -> int: # O(logn), O(1)
         seat_number = heapq.heappop(self.available_seats)
         return seat_number
         
 
-    def unreserve(self, seatNumber: int) -> None:
+    def unreserve(self, seatNumber: int) -> None: # O(logn), O(1)
         heapq.heappush(self.available_seats, seatNumber)
 
 
