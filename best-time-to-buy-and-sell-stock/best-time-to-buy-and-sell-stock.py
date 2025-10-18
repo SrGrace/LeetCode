a9f1f424f71c0c6dd755014f407bc6410bd8a0ae
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2025-10-18 20:03:45 +0530

    Update best-time-to-buy-and-sell-stock.py

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -10,4 +10,15 @@ class Solution:
             return sell[-1]
         
         return maxProfitAfterKTrans(prices, 1) # O(k*n), O(k)
-            
+
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # O(n), O(1)
+        min_price, max_profit = float(inf), 0
+        for p in prices:
+            min_price = min(min_price, p)
+            max_profit = max(max_profit, p-min_price)
+    
+        return max_profit
